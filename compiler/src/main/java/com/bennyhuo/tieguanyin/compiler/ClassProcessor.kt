bff69c1b37e0f2d0d9dd3f0885bbbb9a3516ce47
bennyhuo <bennyhuo@kotliner.cn>
2018-09-13 21:41:25 +0800

    1. Use kotlin style.

--- compiler/src/main/java/com/bennyhuo/tieguanyin/compiler/ClassProcessor.kt
+++ compiler/src/main/java/com/bennyhuo/tieguanyin/compiler/ClassProcessor.kt
@@ -4,8 +4,8 @@ import com.bennyhuo.tieguanyin.annotations.Builder
 import com.bennyhuo.tieguanyin.annotations.Optional
 import com.bennyhuo.tieguanyin.annotations.Required
 import com.bennyhuo.tieguanyin.compiler.activity.ActivityClass
-import com.bennyhuo.tieguanyin.compiler.basic.entity.OptionalField
 import com.bennyhuo.tieguanyin.compiler.basic.entity.Field
+import com.bennyhuo.tieguanyin.compiler.basic.entity.OptionalField
 import com.bennyhuo.tieguanyin.compiler.fragment.FragmentClass
 import com.bennyhuo.tieguanyin.compiler.utils.Logger
 import com.bennyhuo.tieguanyin.compiler.utils.isSubTypeOf
@@ -25,77 +25,53 @@ class ClassProcessor(val filer: Filer){
     fun process(env: RoundEnvironment){
         parseClass(env)
         parseFields(env)
-        brewFiles()
+        buildFiles()
     }
 
-    private fun brewFiles() {
+    private fun buildFiles() {
         activityClasses.values.map(ActivityClass::builder).forEach { it.build(filer) }
         fragmentClasses.values.map(FragmentClass::builder).forEach { it.build(filer) }
     }
 
     private fun parseFields(env: RoundEnvironment) {
-        for (element in env.getElementsAnnotatedWith(Required::class.java)) {
-            if (!SuperficialValidation.validateElement(element)) continue
-            try {
-                if (element.kind == ElementKind.FIELD) {
-                    val activityClass = activityClasses[element.enclosingElement]
-                    if (activityClass == null) {
-                        val fragmentClass = fragmentClasses[element.enclosingElement]
-                        if (fragmentClass == null) {
-                            Logger.error(element, "Field " + element + " annotated as Required while " + element.enclosingElement + " not annotated.")
-                        } else {
-                            fragmentClass.addSymbol(Field(element as VarSymbol))
-                        }
-                    } else {
-                        activityClass.addSymbol(Field(element as VarSymbol))
-                    }
+        env.getElementsAnnotatedWith(Required::class.java)
+                .filter { SuperficialValidation.validateElement(it) }
+                .filter { it.kind == ElementKind.FIELD }
+                .forEach { element ->
+                    (activityClasses[element.enclosingElement]
+                            ?: fragmentClasses[element.enclosingElement])
+                            ?.addSymbol(Field(element as VarSymbol))
+                            ?: Logger.error(element, "Field " + element + " annotated as Required while " + element.enclosingElement + " not annotated.")
                 }
-            } catch (e: Exception) {
-                Logger.logParsingError(element, Required::class.java, e)
-            }
 
-        }
-
-        for (element in env.getElementsAnnotatedWith(Optional::class.java)) {
-            if (!SuperficialValidation.validateElement(element)) continue
-            try {
-                if (element.kind == ElementKind.FIELD) {
-                    val activityClass = activityClasses[element.enclosingElement]
-                    if (activityClass == null) {
-                        val fragmentClass = fragmentClasses[element.enclosingElement]
-                        if (fragmentClass == null) {
-                            Logger.error(element, "Field " + element + " annotated as Optional while " + element.enclosingElement + " not annotated.")
-                        } else {
-                            fragmentClass.addSymbol(OptionalField(element as VarSymbol))
-                        }
-                    } else {
-                        activityClass.addSymbol(OptionalField(element as VarSymbol))
-                    }
+        env.getElementsAnnotatedWith(Optional::class.java)
+                .filter { SuperficialValidation.validateElement(it) }
+                .filter { it.kind == ElementKind.FIELD }
+                .forEach { element ->
+                    (activityClasses[element.enclosingElement]
+                            ?: fragmentClasses[element.enclosingElement])
+                            ?.addSymbol(OptionalField(element as VarSymbol))
+                            ?: Logger.error(element, "Field " + element + " annotated as Optional while " + element.enclosingElement + " not annotated.")
                 }
-            } catch (e: Exception) {
-                Logger.logParsingError(element, Required::class.java, e)
-            }
-        }
     }
 
     private fun parseClass(env: RoundEnvironment) {
-        for (element in env.getElementsAnnotatedWith(Builder::class.java)) {
-            if (!SuperficialValidation.validateElement(element)) continue
-            try {
-                if (element.kind.isClass) {
-                    if(element.asType().isSubTypeOf("android.app.Activity")){
-                        activityClasses[element] = ActivityClass(element as TypeElement)
-                    } else if(element.asType().isSubTypeOf("android.support.v4.app.Fragment")) {
-                        fragmentClasses[element] = FragmentClass(element as TypeElement)
-                    } else {
-                        Logger.error(element, "Unsupported type: %s", element.simpleName)
+        env.getElementsAnnotatedWith(Builder::class.java)
+                .filter(SuperficialValidation::validateElement)
+                .filter { it.kind.isClass }
+                .forEach { element ->
+                    try {
+                        if (element.asType().isSubTypeOf("android.app.Activity")) {
+                            activityClasses[element] = ActivityClass(element as TypeElement)
+                        } else if (element.asType().isSubTypeOf("android.support.v4.app.Fragment")) {
+                            fragmentClasses[element] = FragmentClass(element as TypeElement)
+                        } else {
+                            Logger.error(element, "Unsupported type: %s", element.simpleName)
+                        }
+                    } catch (e: Exception) {
+                        Logger.logParsingError(element, Builder::class.java, e)
                     }
                 }
-            } catch (e: Exception) {
-                Logger.logParsingError(element, Builder::class.java, e)
-            }
-        }
-
         activityClasses.values.forEach { it.setUpSuperClass(activityClasses) }
     }
 }
