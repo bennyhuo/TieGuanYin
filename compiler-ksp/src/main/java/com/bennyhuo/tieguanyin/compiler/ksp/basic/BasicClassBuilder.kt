ea01306ab88ae8d17025db4d819349280e12c0af
bennyhuo <bennyhuo@kotliner.cn>
2021-10-03 12:37:00 +0800

    Compiles to generate Java & Kotlin files. But there are still some problems handling Java types.

--- compiler-ksp/src/main/java/com/bennyhuo/tieguanyin/compiler/ksp/basic/BasicClassBuilder.kt
+++ compiler-ksp/src/main/java/com/bennyhuo/tieguanyin/compiler/ksp/basic/BasicClassBuilder.kt
@@ -2,33 +2,37 @@ package com.bennyhuo.tieguanyin.compiler.ksp.basic
 
 import com.bennyhuo.tieguanyin.compiler.ksp.basic.types.GENERATED_ANNOTATION
 import com.bennyhuo.tieguanyin.compiler.ksp.core.KspContext
+import com.google.devtools.ksp.processing.Dependencies
 import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.JavaFile
 import com.squareup.javapoet.TypeSpec
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.ksp.writeTo
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
 import javax.lang.model.element.Modifier
 
-abstract class BasicClassBuilder(private val basicClass: BasicClass){
+abstract class BasicClassBuilder(private val basicClass: BasicClass) {
 
-//    private fun writeJavaToFile(filer: Filer, typeSpec: TypeSpec) {
-//        try {
-//            val file = JavaFile.builder(basicClass.packageName, typeSpec).build()
-//            file.writeTo(filer)
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//    }
+    private fun writeJavaToFile(typeSpec: TypeSpec) {
+        val javaFile = JavaFile.builder(basicClass.packageName, typeSpec).build()
+        val file = KspContext.environment.codeGenerator.createNewFile(
+            Dependencies(false), basicClass.packageName, typeSpec.name, "java"
+        )
+        OutputStreamWriter(file, StandardCharsets.UTF_8).use(javaFile::writeTo)
+    }
 
     private fun writeKotlinToFile(fileSpec: FileSpec) {
         fileSpec.writeTo(KspContext.environment.codeGenerator, false)
     }
 
-    fun build(){
+    fun build() {
         if (basicClass.isAbstract) return
 
         val typeBuilder = TypeSpec.classBuilder(basicClass.builderClassName)
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addAnnotation(GENERATED_ANNOTATION.java as ClassName)
+            .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+            .addAnnotation(GENERATED_ANNOTATION.java as ClassName)
 
         buildCommon(typeBuilder)
 
@@ -37,7 +41,7 @@ abstract class BasicClassBuilder(private val basicClass: BasicClass){
         buildKotlinBuilders(fileSpecBuilder)
         writeKotlinToFile(fileSpecBuilder.build())
 
-        //writeJavaToFile(filer, typeBuilder.build())
+        writeJavaToFile(typeBuilder.build())
     }
 
     abstract fun buildCommon(typeBuilder: TypeSpec.Builder)
